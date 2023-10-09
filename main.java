ab0a8f51fb9fafbc888ee4948b7c30ac27728338
Gonzalo <gonzalocm@gonzalocm.com>
2023-10-09 10:25:58 +0200

    main now has an addition function

--- main.java
+++ main.java
@@ -3,4 +3,13 @@ public static void Main(String[] args) {
 	System.out.println(salutation);
 	String farewell = "Adiós con el corazón";
 	System.out.println(farewell);
+
+	int num1 = 4;
+	int num2 = 5;
+	System.out.println(addNumbers(num1, num2));
 }
+
+public static int addNumbers(int e, int b) {
+	return (a + b);
+}
+
